1627f932059664ed3386a8e18870ba812a6f418e
Eddie Quan <therealeddiequan@gmail.com>
2016-11-04 15:55:53 -0400

    Adds Course route.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,3 @@
 class Course < ApplicationRecord
-  validates :course_id, :course_code, :semester, :presence => true
-  validates_uniqueness_of :course_id
-  validates :course_code, uniqueness: {scope: [:semster, :professor]}
-
-  has_many :requests
-
-  belongs_to :tooks
+  validates :course_code, :presence => true
 end
