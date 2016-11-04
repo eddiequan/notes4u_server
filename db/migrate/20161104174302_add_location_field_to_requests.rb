04f281b1856f99b587870b8c094d9ff6a54defe6
Eddie Quan <therealeddiequan@gmail.com>
2016-11-04 16:26:59 -0400

    tries to fix migration

--- db/migrate/20161104174302_add_location_field_to_requests.rb
+++ db/migrate/20161104174302_add_location_field_to_requests.rb
@@ -1,7 +1,7 @@
 class AddLocationFieldToRequests < ActiveRecord::Migration[5.0]
   def change
     change_table :requests do |t|
-      t.remove :location, :user_id
+      t.remove :user_id
       t.string :location
       t.integer :user_id
     end
