7a2a36c3ae594a3d4d86c1cfe09109e9407fe3f4
hasun1226 <hasun1226@github.com>
2016-11-09 22:47:49 -0800

    nested resources routed

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-  before_action :set_users
+  before_action :get_users
   before_action :set_request, only: [:show, :update, :destroy]
 
   # GET /requests
@@ -45,7 +45,7 @@ class RequestsController < ApplicationController
       @request = Request.find(params[:id])
     end
 
-    def set_users
+    def get_users
       @user = User.find(params[:user_id])
 
     # Only allow a trusted parameter "white list" through.
