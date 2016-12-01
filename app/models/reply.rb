4320005733608d3327e5fdfbb961c666be7fc2db
Eddie Quan <therealeddiequan@gmail.com>
2016-12-01 17:41:10 -0500

    adds validation to Reply.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,9 @@
 class Reply < ApplicationRecord
+  validate :is_not_already_accepted, on: :create
+
+  def is_not_already_accepted
+    if Request.where(id: request_id).first.status == 2 then
+      errors.add(:request, "has already been accepted.")
+    end
+  end
 end
